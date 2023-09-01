d9a0911b8c8770f7702c268648024767721008b1
keipy27 <kevinandris27@gmail.com>
2023-09-02 11:50:28 +1200

    crud.js initial implementation is created in routes folder

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
+// ! requires
+const connectDb = require('./db/connect')
 const express = require('express')
+const router = require('./routes/crud')
+require('dotenv').config()
 
 const app = express()
 const port = 5000;
-const connectDb = require('./db/connect')
 
-// ! dotenv
-require('dotenv').config()
 
 // ! Middleware
 app.use(express.json())
+
+// ! Routes
+app.use('/api/v1/crud', router)
 
 // ! Connection
 const start = async () => {
