7b80ab3f02ee121c4403f5fdc2771f4f9a368bda
keipy27 <kevinandris27@gmail.com>
2023-09-02 14:08:31 +1200

    added the style for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
+import { styled } from 'styled-components';
 import TodoForm from './components/TodoForm';
 
 function App() {
   return (
-    <div className="App">
-      <TodoForm />
-    </div>
+    <AppStyled>
+      <div className="content-container">
+        <TodoForm />
+      </div>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .content-container {
+    background-color: #374954;
+    width: 80%;
+    min-height: 80vh;
+    border-radius: 20px;
+    box-shadow: 10px 12px 20px rgba(0,0,0, .2);
+  }
+`
 
 export default App;
