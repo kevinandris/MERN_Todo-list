5ac3cc2f07ae8277407193f65dfa417b2e216caf
keipy27 <kevinandris27@gmail.com>
2023-09-02 13:45:04 +1200

    deleted unnecessary files in frontend and imported images localted in img folder

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,17 @@ const connectDb = require('./db/connect')
 const express = require('express')
 const router = require('./routes/crud')
 require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 const port = 5000;
 
 // ! Middleware
 app.use(express.json())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control=Allow-Origin', '*')
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-
-    next();
-})
 
 // ! Routes
 app.use('/api/v1/crud', router)
